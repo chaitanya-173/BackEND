b6be0993d9850721a68b605662e6c2e5e35bef4e
chaitanya-173 <chaitanyachaudhary73@gmail.com>
2025-04-11 17:40:49 +0530

    First Node Server created

--- 01. First Node Server/app.js
+++ 01. First Node Server/app.js
@@ -1,7 +1,30 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+
+  if (req.url === '/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>NodeJS</title></head>');
+    res.write('<body><h1>Welcome to Home</h1></body>');
+    res.write('</html>');
+    return res.end(); 
+  } else if (req.url === '/products') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>NodeJS</title></head>');
+    res.write('<body><h1>Checkout our Products</h1></body>');
+    res.write('</html>');
+    return res.end();
+  }
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>NodeJS</title></head>');
+  res.write('<body><h1>Complete MERN Stack</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 const PORT = 3000;
