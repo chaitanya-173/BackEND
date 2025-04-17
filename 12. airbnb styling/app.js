2b63619179153a3afd52225b2209571bf1c1adec
chaitanya-173 <chaitanyachaudhary73@gmail.com>
2025-04-17 22:50:55 +0530

    serving static files

---
+++ 12. airbnb styling/app.js
@@ -0,0 +1,26 @@
+// Core Modules
+const path = require('path');
+
+// External Modules
+const express = require('express');
+
+// Local Modules
+const userRouter = require('./routes/userRouter');
+const hostRouter = require('./routes/hostRouter');
+const rootDir = require('./utils/pathUtils');
+
+const app = express();
+
+app.use(express.urlencoded());
+
+app.use(userRouter);
+app.use("/host", hostRouter);
+
+app.use(express.static(path.join(rootDir, 'public')));
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
