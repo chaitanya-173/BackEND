70d619aa8f9bbd9d1eb2647d7a675c3f0ac40ba1
chaitanya-173 <chaitanyachaudhary73@gmail.com>
2025-05-14 22:53:52 +0530

    Cookies and Sessions with auth initialisation

---
+++ 20. airbnb Cookies and Sessions/app.js
@@ -0,0 +1,57 @@
+// Core Module
+const path = require("path");
+
+// External Module
+const express = require("express");
+
+//Local Module
+const storeRouter = require("./routes/storeRouter");
+const hostRouter = require("./routes/hostRouter");
+const authRouter = require("./routes/authRouter");
+const rootDir = require("./utils/pathUtil");
+const errorsController = require("./controllers/errors");
+const { default: mongoose } = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(express.urlencoded());
+app.use((req, res, next) => {
+  console.log("Cookie check middleware", req.get("Cookie"));
+  req.isLoggedIn = req.get("Cookie")
+    ? req.get("Cookie").split("=")[1] === "true"
+    : "false";
+  next();
+});
+
+app.use(authRouter);
+app.use(storeRouter);
+app.use("/host", (req, res, next) => {
+  if (req.isLoggedIn) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+});
+app.use("/host", hostRouter);
+
+app.use(express.static(path.join(rootDir, "public")));
+
+app.use(errorsController.pageNotFound);
+
+const PORT = 3000;
+
+mongoose
+  .connect(
+    "mongodb+srv://chaitanyachaudhary73:FGGyrjub7QrPXHNf@chaitanya-cluster.7bkyhy1.mongodb.net/airbnb?retryWrites=true&w=majority&appName=chaitanya-cluster"
+  )
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on address http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error while connecting to Mongo: ", err);
+  });
